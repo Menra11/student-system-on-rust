af3857ebbefe1ca6c8efc98b3cecfb74e934eb40
Menra <112563088+Menra11@users.noreply.github.com>
2025-06-19 16:49:24 +0800

    refactor(fontawesome): dynamically import all solid icons

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,75 +1,13 @@
 // plugins/fontawesome.js
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faUserCircle,
-  faGraduationCap,
-  faBook,
-  faChartBar,
-  faSignOutAlt,
-  faUser,
-  faSpinner,
-  faExclamationCircle,
-  faUserPlus,
-  faQuestionCircle,
-  faSignInAlt,
-  faChevronDown,
-  faLock,
-  faUserGraduate,
-  faChalkboardTeacher,
-  faUserShield,
-  faIdCard,
-  faHashtag,
-  faSignature,
-  faKey,
-  faVenusMars,
-  faMars,
-  faVenus,
-  faCalendarDays,
-  faCalendar,
-  faBuildingColumns,
-  faPhone,
-  faMobileScreen,
-  faEnvelope,
-  faAt,
-} from "@fortawesome/free-solid-svg-icons";
+import * as faIcons from "@fortawesome/free-solid-svg-icons";
 
 // 防止自动添加CSS
 config.autoAddCss = false;
 
 // 添加图标到库
-library.add(
-  faUserCircle,
-  faGraduationCap,
-  faBook,
-  faChartBar,
-  faSignOutAlt,
-  faUser,
-  faSpinner,
-  faExclamationCircle,
-  faUserPlus,
-  faQuestionCircle,
-  faSignInAlt,
-  faChevronDown,
-  faLock,
-  faUserGraduate,
-  faChalkboardTeacher,
-  faUserShield,
-  faIdCard,
-  faHashtag,
-  faSignature,
-  faKey,
-  faVenusMars,
-  faMars,
-  faVenus,
-  faCalendarDays,
-  faCalendar,
-  faBuildingColumns,
-  faPhone,
-  faMobileScreen,
-  faEnvelope,
-  faAt,
-);
+library.add(faIcons.fas);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("font-awesome-icon", FontAwesomeIcon);
